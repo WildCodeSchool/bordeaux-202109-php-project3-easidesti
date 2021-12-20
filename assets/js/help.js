1a7118503334bd730828c2f9e5bdc139b31202f6
Sebastien Juchet <sebastien.juchet@gmail.com>
2021-12-20 22:51:27 +0100

    refactor js files help and phoneme

--- assets/js/help.js
+++ assets/js/help.js
@@ -1,7 +1,5 @@
-if (document.getElementById('help-listen')) {
-    const buttonHelpListen = document.getElementById('help-listen');
-    const audioHelp = document.getElementById('audio-help');
-    buttonHelpListen.addEventListener('click', (event) => {
-        audioHelp.play();
-    });
-}
+const buttonHelpListen = document.getElementById('help-listen');
+const audioHelp = document.getElementById('audio-help');
+buttonHelpListen.addEventListener('click', (event) => {
+    audioHelp.play();
+});
