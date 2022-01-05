ac48e89fdec3d435bca7a6289efeb455259e25e4
Sebastien Juchet <sebastien.juchet@gmail.com>
2022-01-06 00:07:15 +0100

    creat some fixtures for endpoint

--- src/DataFixtures/EndpointFixtures.php
+++ src/DataFixtures/EndpointFixtures.php
@@ -12,68 +12,132 @@ class EndpointFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        /*$endpoint = new Endpoint();
-        $endpoint->setWord($this->getReference('word_haricot'));
-        $endpoint->setPosition(1);
-        $endpoint->setCreatedAt(new DateTime());
+        $endpoint = new Endpoint();
+        $endpoint->setWord($this->getReference('letter_a_serie_1_word_0'));
+        $endpoint->setPosition(2);
         $manager->persist($endpoint);
 
         $endpoint1 = new Endpoint();
-        $endpoint1->setWord($this->getReference('word_haricot'));
-        $endpoint1->setPosition(3);
-        $endpoint1->setCreatedAt(new DateTime());
+        $endpoint1->setWord($this->getReference('letter_a_serie_1_word_0'));
+        $endpoint1->setPosition(5);
         $manager->persist($endpoint1);
 
         $endpoint2 = new Endpoint();
-        $endpoint2->setWord($this->getReference('word_haricot'));
-        $endpoint2->setPosition(6);
-        $endpoint2->setCreatedAt(new DateTime());
+        $endpoint2->setWord($this->getReference('letter_a_serie_1_word_1'));
+        $endpoint2->setPosition(1);
         $manager->persist($endpoint2);
 
         $endpoint3 = new Endpoint();
-        $endpoint3->setWord($this->getReference('word_banc'));
+        $endpoint3->setWord($this->getReference('letter_a_serie_1_word_1'));
         $endpoint3->setPosition(3);
-        $endpoint3->setCreatedAt(new DateTime());
         $manager->persist($endpoint3);
 
         $endpoint4 = new Endpoint();
-        $endpoint4->setWord($this->getReference('word_pain'));
-        $endpoint4->setPosition(3);
-        $endpoint4->setCreatedAt(new DateTime());
+        $endpoint4->setWord($this->getReference('letter_a_serie_1_word_2'));
+        $endpoint4->setPosition(1);
         $manager->persist($endpoint4);
 
         $endpoint5 = new Endpoint();
-        $endpoint5->setWord($this->getReference('word_jambe'));
-        $endpoint5->setPosition(2);
-        $endpoint5->setCreatedAt(new DateTime());
+        $endpoint5->setWord($this->getReference('letter_a_serie_1_word_2'));
+        $endpoint5->setPosition(3);
         $manager->persist($endpoint5);
 
         $endpoint6 = new Endpoint();
-        $endpoint6->setWord($this->getReference('word_jambe'));
-        $endpoint6->setPosition(4);
-        $endpoint6->setCreatedAt(new DateTime());
+        $endpoint6->setWord($this->getReference('letter_a_serie_1_word_3'));
+        $endpoint6->setPosition(1);
         $manager->persist($endpoint6);
 
         $endpoint7 = new Endpoint();
-        $endpoint7->setWord($this->getReference('word_ail'));
-        $endpoint7->setPosition(2);
-        $endpoint7->setCreatedAt(new DateTime());
+        $endpoint7->setWord($this->getReference('letter_a_serie_1_word_3'));
+        $endpoint7->setPosition(4);
         $manager->persist($endpoint7);
 
         $endpoint8 = new Endpoint();
-        $endpoint8->setWord($this->getReference('word_cadeau'));
+        $endpoint8->setWord($this->getReference('letter_a_serie_1_word_4'));
         $endpoint8->setPosition(1);
-        $endpoint8->setCreatedAt(new DateTime());
         $manager->persist($endpoint8);
 
         $endpoint9 = new Endpoint();
-        $endpoint9->setWord($this->getReference('word_cadeau'));
-        $endpoint9->setPosition(5);
-        $endpoint9->setCreatedAt(new DateTime());
+        $endpoint9->setWord($this->getReference('letter_a_serie_1_word_4'));
+        $endpoint9->setPosition(4);
         $manager->persist($endpoint9);
 
+        $endpoint10 = new Endpoint();
+        $endpoint10->setWord($this->getReference('letter_a_serie_1_word_5'));
+        $endpoint10->setPosition(1);
+        $manager->persist($endpoint10);
 
-        $manager->flush();*/
+        $endpoint11 = new Endpoint();
+        $endpoint11->setWord($this->getReference('letter_a_serie_1_word_5'));
+        $endpoint11->setPosition(5);
+        $manager->persist($endpoint11);
+
+        $endpoint12 = new Endpoint();
+        $endpoint12->setWord($this->getReference('letter_a_serie_1_word_6'));
+        $endpoint12->setPosition(2);
+        $manager->persist($endpoint12);
+
+        $endpoint13 = new Endpoint();
+        $endpoint13->setWord($this->getReference('letter_a_serie_1_word_7'));
+        $endpoint13->setPosition(1);
+        $manager->persist($endpoint13);
+
+        $endpoint14 = new Endpoint();
+        $endpoint14->setWord($this->getReference('letter_a_serie_1_word_8'));
+        $endpoint14->setPosition(1);
+        $manager->persist($endpoint14);
+
+        $endpoint15 = new Endpoint();
+        $endpoint15->setWord($this->getReference('letter_a_serie_1_word_8'));
+        $endpoint15->setPosition(3);
+        $manager->persist($endpoint15);
+
+        $endpoint16 = new Endpoint();
+        $endpoint16->setWord($this->getReference('letter_a_serie_1_word_8'));
+        $endpoint16->setPosition(5);
+        $manager->persist($endpoint16);
+
+        $endpoint17 = new Endpoint();
+        $endpoint17->setWord($this->getReference('letter_a_serie_1_word_9'));
+        $endpoint17->setPosition(1);
+        $manager->persist($endpoint17);
+
+        $endpoint18 = new Endpoint();
+        $endpoint18->setWord($this->getReference('letter_a_serie_1_word_9'));
+        $endpoint18->setPosition(4);
+        $manager->persist($endpoint18);
+
+        $endpoint19 = new Endpoint();
+        $endpoint19->setWord($this->getReference('letter_a_serie_1_word_10'));
+        $endpoint19->setPosition(0);
+        $manager->persist($endpoint19);
+
+        $endpoint20 = new Endpoint();
+        $endpoint20->setWord($this->getReference('letter_a_serie_1_word_10'));
+        $endpoint20->setPosition(4);
+        $manager->persist($endpoint20);
+
+        $endpoint21 = new Endpoint();
+        $endpoint21->setWord($this->getReference('letter_a_serie_1_word_11'));
+        $endpoint21->setPosition(0);
+        $manager->persist($endpoint21);
+
+        $endpoint22 = new Endpoint();
+        $endpoint22->setWord($this->getReference('letter_a_serie_1_word_11'));
+        $endpoint22->setPosition(4);
+        $manager->persist($endpoint22);
+
+        $endpoint23 = new Endpoint();
+        $endpoint23->setWord($this->getReference('letter_a_serie_1_word_12'));
+        $endpoint23->setPosition(1);
+        $manager->persist($endpoint23);
+
+        $endpoint24 = new Endpoint();
+        $endpoint24->setWord($this->getReference('letter_a_serie_1_word_12'));
+        $endpoint24->setPosition(4);
+        $manager->persist($endpoint24);
+
+        $manager->flush();
     }
     public function getDependencies()
     {
