1372fea2b2f1426524f91f8e0a7ce17b5a975a64
joffrey-gibouin <joffreygibouin@gmail.com>
2022-01-10 14:41:26 +0100

    add fixture pronunciation

--- src/DataFixtures/PronunciationFixtures.php
+++ src/DataFixtures/PronunciationFixtures.php
@@ -46,6 +46,108 @@ class PronunciationFixtures extends Fixture
         $this->addReference('pronunciation_ambulance', $pronunciation6);
         $manager->persist($pronunciation6);
 
+        $pronunciation7 = new Pronunciation();
+        $pronunciation7->setName('cerise');
+        $pronunciation7->setPicture('lettre_e_1.png');
+        $this->addReference('pronunciation_cerise', $pronunciation7);
+        $manager->persist($pronunciation7);
+
+        $pronunciation8 = new Pronunciation();
+        $pronunciation8->setName('échelle');
+        $pronunciation8->setPicture('lettre_e_2.png');
+        $this->addReference('pronunciation_échelle', $pronunciation8);
+        $manager->persist($pronunciation8);
+
+        $pronunciation9 = new Pronunciation();
+        $pronunciation9->setName('eau');
+        $pronunciation9->setPicture('lettre_e_3.png');
+        $this->addReference('pronunciation_eau', $pronunciation9);
+        $manager->persist($pronunciation9);
+
+        $pronunciation10 = new Pronunciation();
+        $pronunciation10->setName('ceinture');
+        $pronunciation10->setPicture('lettre_e_4.png');
+        $this->addReference('pronunciation_ceinture', $pronunciation10);
+        $manager->persist($pronunciation10);
+
+        $pronunciation11 = new Pronunciation();
+        $pronunciation11->setName('entonnoir');
+        $pronunciation11->setPicture('lettre_e_5.png');
+        $this->addReference('pronunciation_entonnoir', $pronunciation11);
+        $manager->persist($pronunciation11);
+
+        $pronunciation12 = new Pronunciation();
+        $pronunciation12->setName('poêle');
+        $pronunciation12->setPicture('lettre_e_6.png');
+        $this->addReference('pronunciation_poêle', $pronunciation12);
+        $manager->persist($pronunciation12);
+
+        $pronunciation13 = new Pronunciation();
+        $pronunciation13->setName('abeille');
+        $pronunciation13->setPicture('lettre_e_7.png');
+        $this->addReference('pronunciation_abeille', $pronunciation13);
+        $manager->persist($pronunciation13);
+
+        $pronunciation14 = new Pronunciation();
+        $pronunciation14->setName('feu');
+        $pronunciation14->setPicture('lettre_e_8.png');
+        $this->addReference('pronunciation_feu', $pronunciation14);
+        $manager->persist($pronunciation14);
+
+        $pronunciation15 = new Pronunciation();
+        $pronunciation15->setName('île');
+        $pronunciation15->setPicture('lettre_i_1.png');
+        $this->addReference('pronunciation_île', $pronunciation15);
+        $manager->persist($pronunciation15);
+
+        $pronunciation16 = new Pronunciation();
+        $pronunciation16->setName('écureuil');
+        $pronunciation16->setPicture('lettre_i_2.png');
+        $this->addReference('pronunciation_écureuil', $pronunciation16);
+        $manager->persist($pronunciation16);
+
+        $pronunciation17 = new Pronunciation();
+        $pronunciation17->setName('pain');
+        $pronunciation17->setPicture('lettre_i_3.png');
+        $this->addReference('pronunciation_pain_i', $pronunciation17);
+        $manager->persist($pronunciation17);
+
+        $pronunciation18 = new Pronunciation();
+        $pronunciation18->setName('oiseau');
+        $pronunciation18->setPicture('lettre_i_4.png');
+        $this->addReference('pronunciation_oiseau', $pronunciation18);
+        $manager->persist($pronunciation18);
+
+        $pronunciation19 = new Pronunciation();
+        $pronunciation19->setName('poing');
+        $pronunciation19->setPicture('lettre_i_5.png');
+        $this->addReference('pronunciation_poing', $pronunciation19);
+        $manager->persist($pronunciation19);
+
+        $pronunciation20 = new Pronunciation();
+        $pronunciation20->setName('serpent');
+        $pronunciation20->setPicture('lettre_s_1.png');
+        $this->addReference('pronunciation_serpent', $pronunciation20);
+        $manager->persist($pronunciation20);
+
+        $pronunciation21 = new Pronunciation();
+        $pronunciation21->setName('poison');
+        $pronunciation21->setPicture('lettre_s_2.png');
+        $this->addReference('pronunciation_poison', $pronunciation21);
+        $manager->persist($pronunciation21);
+
+        $pronunciation22 = new Pronunciation();
+        $pronunciation22->setName('chat');
+        $pronunciation22->setPicture('lettre_s_3.png');
+        $this->addReference('pronunciation_chat', $pronunciation22);
+        $manager->persist($pronunciation22);
+
+        $pronunciation24 = new Pronunciation();
+        $pronunciation24->setName('muet');
+        $pronunciation24->setPicture('lettre_s_4.png');
+        $this->addReference('pronunciation_muet', $pronunciation24);
+        $manager->persist($pronunciation24);
+
         $manager->flush();
     }
 }
