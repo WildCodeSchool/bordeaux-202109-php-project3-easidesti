77dbec46b4f65ea3788382aa011aa461a5417a74
joffrey-gibouin <joffreygibouin@gmail.com>
2022-01-24 11:53:52 +0100

    add Vich_uploader

--- src/Entity/Word.php
+++ src/Entity/Word.php
@@ -7,10 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use DateTime;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
 
 /**
  * @ORM\Entity(repositoryClass=WordRepository::class)
  * @ORM\HasLifecycleCallbacks()
+ * @Vich\Uploadable
  */
 class Word
 {
@@ -52,9 +55,27 @@ class Word
     private DateTime $createdAt;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="word_image", fileNameProperty="picture")
+     *
+     * @var File
      */
-    private ?string $picture;
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @var string
+     */
+    private $picture;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     *
+     * @var \DateTimeInterface
+     */
+    private $updatedAt;
 
     /**
      * @ORM\ManyToOne(targetEntity=Letter::class, inversedBy="words")
@@ -81,6 +102,41 @@ class Word
     {
         $this->muteLetters = new ArrayCollection();
         $this->endpoints = new ArrayCollection();
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
     }
 
     public function getId(): ?int
@@ -123,6 +179,24 @@ class Word
 
         return $this;
     }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getUpdatedAt(): \DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTimeInterface $updatedAt
+     */
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+
 
     /**
      * @return Collection|MuteLetter[]
--- src/Form/WordType.php
+++ src/Form/WordType.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 use function PHPUnit\Framework\isNull;
 
@@ -41,11 +42,12 @@ class WordType extends AbstractType
                     'class'    => 'd-flex col-4',
                 ]
             ])
-            ->add('picture', FileType::class, [
-                'label'    => 'Image',
-                'required' => false,
+            ->add('imageFile', VichFileType::class, [
+                'label'        => 'image du mot',
+                'required'      => false,
+                'allow_delete'  => true, // not mandatory, default is true
+                'download_uri' => true, // not mandatory, default is true
             ])
-
 
             ->add('level', EntityType::class, [
                 'class'        => Serie::class,
